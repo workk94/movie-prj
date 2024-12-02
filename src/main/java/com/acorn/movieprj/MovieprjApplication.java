f3b903f0367a58e972e4059b685be1b13a0843d0
workk94 <workk219@gmail.com>
2024-12-02 09:36:41 +0900

    first commit

---
+++ src/main/java/com/acorn/movieprj/MovieprjApplication.java
@@ -0,0 +1,13 @@
+package com.acorn.movieprj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieprjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieprjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acorn/movieprj/MovieprjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acorn.movieprj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieprjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
